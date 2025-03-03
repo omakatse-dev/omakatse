ea77fb9b30180e822cd1277548bd36715d786cc9
Darren Teo HH <darrenteo77@gmail.com>
2025-03-03 15:13:29 +0800

    Work on (style) NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -32,23 +32,32 @@ export default function NavBar() {
   ];
   return (
     <div className="flex justify-between bg-yellow py-3 px-6 rounded-full fixed top-4 w-11/12 left-1/2 -translate-x-1/2">
+
       <Image src="/assets/omakatsehorizontalblack.svg" alt="Logo" width={120} height={120} />
-      <div className="flex items-center gap-x-8">
+
+      <div className="flex items-center gap-x-8 text-xs font-open-sans font-semibold">
         {links.map((link) => (
           <Link href={link.url} key={link.name}>
             {link.name}
           </Link>
         ))}
-        <button>Build your box now</button>
-        <button>
-          <MagnifyingGlassIcon className="h-4 w-4 stroke-black stroke-[2]" />
+
+        <button className="bg-white rounded-full h-8 font-open-sans font-semibold text-[10px] flex items-center justify-center px-8 outline-1 outline-black">
+          Build your box now
         </button>
-        <button>
-          <UserIcon className="h-4 w-4 stroke-black stroke-[2]" />
-        </button>
-        <button>
-          <ShoppingCartIcon className="h-4 w-4 stroke-black stroke-[2]" />
-        </button>
+
+        <div className="flex gap-4">
+          <button>
+            <MagnifyingGlassIcon className="h-4 w-4 stroke-black stroke-[2]" />
+          </button>
+          <button>
+            <UserIcon className="h-4 w-4 stroke-black stroke-[2]" />
+          </button>
+          <button>
+            <ShoppingCartIcon className="h-4 w-4 stroke-black stroke-[2]" />
+          </button>
+        </div>
+
       </div>
     </div>
   );
