f99b267018700f52cc1576fe8e5c2fd67de086e3
matthew-chua <mattchua2603@gmail.com>
2025-03-04 15:57:16 +0800

    added getProductsByCollection

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,12 @@
-import { getStoreFront } from "../utils/APIs";
+import { getProductsByCollection, getStoreFront } from "../utils/APIs";
 
 import React from "react";
 import { ProductInterface } from "../utils/Interfaces";
 
 export default async function Page() {
   const items = await getStoreFront();
-  console.log(items);
+  const test = await getProductsByCollection("441616957699")
+  console.log(test);
   return (
     <div>
       <h1>Products</h1>
--- src/app/utils/APIs.ts
+++ src/app/utils/APIs.ts
@@ -2,14 +2,15 @@ import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 import { LATEST_API_VERSION } from "@shopify/shopify-api";
 import { ProductEdgeInterface } from "./Interfaces";
 
-export const getStoreFront = async () => {
-  const storefrontClient = createStorefrontApiClient({
-      storeDomain: process.env.NEXT_PUBLIC_API_URL || "",
-      apiVersion: LATEST_API_VERSION,
-      publicAccessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
-    });
+const storefrontClient = createStorefrontApiClient({
+  storeDomain: process.env.NEXT_PUBLIC_API_URL || "",
+  apiVersion: LATEST_API_VERSION,
+  publicAccessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+});
 
-    const productQuery = `{
+export const getStoreFront = async () => {
+
+  const productQuery = `{
       products(first: 3) {
         edges {
           node {
@@ -23,4 +24,24 @@ export const getStoreFront = async () => {
 
   const { data } = await storefrontClient.request(productQuery);
   return data.products.edges.map((edge: ProductEdgeInterface) => edge.node);
+}
+
+export const getProductsByCollection = async (collectionID: string) => {
+
+  const productQuery = `{
+  collection(id: "gid://shopify/Collection/${collectionID}") {
+    title
+    products(first: 100) {
+      edges {
+        node {
+          id
+          title
+        }
+      }
+    }
+  }
+}`;
+
+  const { data } = await storefrontClient.request(productQuery);
+  return data.collection.products;
 }
