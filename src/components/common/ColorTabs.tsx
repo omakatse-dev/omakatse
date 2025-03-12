3582440689ae8776917addf0b6ebb3c19c1d9c06
Darren Teo HH <darrenteo77@gmail.com>
2025-03-12 16:55:20 +0800

    Add ColorTabs component

---
+++ src/components/common/ColorTabs.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function ColorTabs({
+  tabs,
+  selectedTab,
+  onChange,
+  className,
+}: {
+  tabs: string[];
+  selectedTab: string;
+  onChange: (tab: string) => void;
+  className?: string;
+}) {
+  return (
+    <div className={`${className} flex gap-4 bodyMD`}>
+      {tabs.map((tab) => (
+        <div
+          key={tab}
+          className={`w-10 h-10 rounded-full cursor-pointer border mb-1 ${
+            tab === selectedTab ? "border-black" : "border-transparent"
+          }`}
+          onClick={() => onChange(tab)}
+          style={{
+            backgroundColor: `#${tab}`,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+}
--- src/components/shop/ProductDetails.tsx
+++ src/components/shop/ProductDetails.tsx
@@ -3,12 +3,15 @@
 import React from 'react'
 import { useState } from 'react'
 import ProductTabs from '../common/ProductTabs'
+import ColorTabs from '../common/ColorTabs'
 
 export default function ProductDetails({ productID }: {
   productID: String
 }) {
 
   const [selectedTab, setSelectedTab] = useState("Small Box");
+  const [selectedColor, setSelectedColor] = useState("40AED7");
+
 
   return (
     <div>
@@ -17,6 +20,12 @@ export default function ProductDetails({ productID }: {
         tabs={["Option one", "Option two", "Option three"]}
         selectedTab={selectedTab}
         onChange={setSelectedTab}
+        className=""
+      />
+      <ColorTabs
+        tabs={["40AED7", "EE807F"]}
+        selectedTab={selectedColor}
+        onChange={setSelectedColor}
         className=""
       />
     </div>
