3cc0f099536b61f70b3d42a439b25f2fce9e1c80
Darren Teo HH <darrenteo77@gmail.com>
2025-03-12 16:08:21 +0800

    Work on product page - Add ProductTabs component for reusable tabs for product page - Add ProductDetails page for client side page

--- src/app/shop/[productId]/page.tsx
+++ src/app/shop/[productId]/page.tsx
@@ -1,7 +1,12 @@
+import ProductDetails from "@/components/shop/ProductDetails";
+
 export default async function ProductPage({ params }: { params: { productId: string } }) {
     const { productId } = await params;
 
     return (
-        <div className='mt-32'>{productId}</div>
+        <div className='mt-32'>
+            {productId}
+            <ProductDetails/>
+        </div>
     )
-}
+} 
