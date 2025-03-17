e44d1362203653ed7f4e9cc179ec366a0dac3d57
Darren Teo HH <darrenteo77@gmail.com>
2025-03-17 11:43:48 +0800

    Introduced state and make images in ProductImages buttons that can change the main Image shown

--- src/components/shop/ProductPage/ProductImages.tsx
+++ src/components/shop/ProductPage/ProductImages.tsx
@@ -1,16 +1,26 @@
+"use client";
+
 import { ProductImageNode } from "@/types/Types";
 import Image from "next/image";
-import React from "react";
+import React , { useState } from "react";
 
 export default function ProductImages({
   images,
 }: {
   images: ProductImageNode;
 }) {
+  const [selectedImage, setSelectedImage] = useState(images.nodes[0].url);
   return (
     <div className="flex flex-row gap-5 w-full">
       <div className="flex flex-col gap-5 w-1/8">
         {images.nodes.map((image) => (
+        <button
+          key={image.url}
+          onClick={() => setSelectedImage(image.url)}
+          className={`border-2 p-1 rounded-xl ${
+            selectedImage === image.url ? "border-primary" : "border-transparent"
+          }`}
+        >
           <Image
             src={image.url}
             alt="product"
@@ -19,10 +29,11 @@ export default function ProductImages({
             height={80}
             className="rounded-xl border-primary"
           />
-        ))}
+        </button>
+      ))}
       </div>
       <Image
-        src={images.nodes[0].url}
+        src={selectedImage}
         alt="product"
         className="bg-gray-500 rounded-xl w-7/8 aspect-square border-primary"
         width={516}
