af561d31398ffeebafd7085977c15c70ea1c1c31
Darren Teo HH <darrenteo77@gmail.com>
2025-04-04 01:24:19 +0800

    Minor change

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
     return (
-        <div className='mt-32'>coming soon page</div>
+        <div className='my-40'>Subscription Page is coming soon!</div>
     )
 }
