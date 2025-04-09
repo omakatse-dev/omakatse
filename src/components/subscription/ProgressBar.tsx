1f2e6059957b9a6e66789395581b24eca7ccfdd3
Darren Teo HH <darrenteo77@gmail.com>
2025-04-09 14:06:24 +0800

    Progress Bar changes color when unlocked

--- src/components/subscription/ProgressBar.tsx
+++ src/components/subscription/ProgressBar.tsx
@@ -10,6 +10,7 @@ export default function ProgressBar({
   className?: string;
 }) {
   const fill = Number(((currentStep / totalSteps) * 100).toFixed(0));
+  const progressBarColor = fill === 100 ? "bg-yellow" : "bg-gray-200"; // Change color when 100%
   return (
     <div className={`flex flex-col items-center w-full gap-2 ${className}`}>
       {showSteps && (
@@ -20,7 +21,7 @@ export default function ProgressBar({
       <div className="h-2.5 bg-gray-50 w-full rounded-full border-secondary">
         {fill > 0 && (
           <div
-            className={`h-full bg-gray-200 rounded-full border-r border-gray-400`}
+            className={`h-full ${progressBarColor} rounded-full border-r border-gray-400`}
             style={{ width: `${fill}%` }}
           />
         )}
