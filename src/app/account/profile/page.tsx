17c1060b3f7e6987c27acc5888e37e46f0ac1e9c
matthew-chua <mattchua2603@gmail.com>
2025-04-11 17:31:16 +0800

    added contact card

--- src/app/account/profile/page.tsx
+++ src/app/account/profile/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
+import Card from "@/components/common/Card";
 import { useUser } from "@auth0/nextjs-auth0/client";
+import Link from "next/link";
 // import { getAccessToken } from "@auth0/nextjs-auth0";
-import ShippingAddress from "@/components/account/profile/ShippingAddress";
-import { getCustomerShippingDetails } from "@/utils/APIs";
-import { useState, useEffect } from "react";
 
 export default function AccountProfilePage() {
   const { user, isLoading } = useUser();
-  const [customerDetails, setCustomerDetails] = useState(null);
-
-  useEffect(() => {
-    if (!user?.email) return;
-
-    const fetchDetails = async () => {
-      const res = await getCustomerShippingDetails(user.email as string);
-      setCustomerDetails(res);
-    };
-
-    fetchDetails();
-  }, [user?.email]);
-  console.log(customerDetails);
 
   // const getToken = async () => {
   //   const token = await getAccessToken();
@@ -31,13 +17,28 @@ export default function AccountProfilePage() {
 
   return (
     <div className="w-full flex flex-col">
-      <h2 className="hidden sm:flex">Profile</h2>
+      <h2 className="hidden lg:flex">Profile</h2>
       <h3 className="font-bold sm:mt-10">Account Details</h3>
       {/* <a href="/api/auth/logout">Logout</a> */}
       <div className="bodyMD text-gray-500 mt-6">Email</div>
       <div className="bodyMD">{user?.email}</div>
-      <hr className="border border-gray-200 my-8" />
-      <ShippingAddress />
+      <Card
+        variant="grey"
+        className="bg-white mt-10 flex flex-col md:flex-row justify-between md:items-center"
+      >
+        <div className="flex flex-col">
+          <h4>Need assistance for your account?</h4>
+          <div className="bodyMD font-semibold">
+            You can reach us to update your address or subscription matters
+          </div>
+        </div>
+        <Link
+          href="/contact"
+          className="underline underline-offset-8 bodyButton mt-4 md:mt-0"
+        >
+          Contact Us
+        </Link>
+      </Card>
     </div>
   );
 }
