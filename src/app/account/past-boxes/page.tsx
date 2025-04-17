ef83f1f4cbe1f77b7e283f27cefb5e07d7c724ab
Darren Teo HH <darrenteo77@gmail.com>
2025-04-17 17:42:59 +0800

    Add Past Boxes boiler plate

---
+++ src/app/account/past-boxes/page.tsx
@@ -0,0 +1,8 @@
+import React, { Suspense } from "react";
+export default function PastBoxesPage() {
+  return (
+    <div>
+      <h2 className="hidden lg:block">Past Boxes</h2>
+    </div>
+  );
+}
