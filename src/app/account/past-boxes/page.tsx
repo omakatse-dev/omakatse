2888db4b9cfd370a52c184c2555dabba08d1c602
Darren Teo HH <darrenteo77@gmail.com>
2025-04-17 18:06:16 +0800

    Work on past boxes page

--- src/app/account/past-boxes/page.tsx
+++ src/app/account/past-boxes/page.tsx
@@ -1,8 +1,12 @@
-import React, { Suspense } from "react";
+import React from "react";
+import PastBoxCard from "@/components/subscription/PastBoxCard";
+
 export default function PastBoxesPage() {
   return (
     <div>
       <h2 className="hidden lg:block">Past Boxes</h2>
+      {/* here, i will have to get all boxes under each subscription (contractid) and pass to each card*/}
+      <PastBoxCard />
     </div>
   );
 }
