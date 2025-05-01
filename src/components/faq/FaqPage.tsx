ce46ab4bf3eb29a628f96e8c72510f24dc0bd3d1
Darren Teo HH <darrenteo77@gmail.com>
2025-05-01 22:13:58 +0800

    Fix FAQ page

--- src/components/faq/FaqPage.tsx
+++ src/components/faq/FaqPage.tsx
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
   Transition,
-  CloseButton,
-} from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { EntryFields, Entry, ChainModifiers } from "contentful";
-import ScrollUpButton from "../common/ScrollUpButton";
+  CloseButton
+} from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
+import { EntryFields, Entry, ChainModifiers } from 'contentful';
+import ScrollUpButton from '../common/ScrollUpButton';
 
 export type faqType = {
-  contentTypeId: "faq";
+  contentTypeId: 'faq';
   fields: {
     category: EntryFields.Text;
     question: EntryFields.Text;
@@ -22,33 +22,35 @@ export type faqType = {
 };
 
 export default function Page({
-  faqs,
+  faqs
 }: {
   faqs: Entry<faqType, ChainModifiers, string>[];
 }) {
-  const categoryRefs = React.useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const categoryRefs = React.useRef<{ [key: string]: HTMLDivElement | null }>(
+    {}
+  );
 
-  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+  const [selectedCategory, setSelectedCategory] = useState<string>('All');
 
   const handleCategoryClick = (category: string) => {
     setSelectedCategory(category);
-  
+
     if (category === 'All') {
       window.scrollTo({ top: 0, behavior: 'smooth' });
       return;
     }
     const element = categoryRefs.current[category];
-  
+
     if (element) {
-      const navbarHeight = 190; 
+      const navbarHeight = 190;
       const elementTop = element.getBoundingClientRect().top + window.scrollY;
       window.scrollTo({ top: elementTop - navbarHeight, behavior: 'smooth' });
     }
   };
 
   const categories = [
-    "All",
-    ...new Set(faqs.map((faq) => faq.fields.category)),
+    'All',
+    ...new Set(faqs.map((faq) => faq.fields.category))
   ];
 
   // Group FAQs by category
@@ -63,22 +65,43 @@ export default function Page({
     },
     {}
   );
-  
+
   return (
-    <div className="relative flex flex-col items-center w-full md:px-64">
+    <div className="relative flex w-full flex-col max-w-7xl items-center md:flex-row md:gap-20 md:px-64 md:mr-60">
+
+      {/* Desktop Sidebar Navigation */}
+      <div className="sticky top-40 mt-6 hidden flex-col gap-4 self-start md:flex w-fit">
+        {categories.map((category) => (
+          <button
+            key={category.toString()}
+            className="bodyMD text-primary text-left whitespace-nowrap"
+            onClick={() => handleCategoryClick(category.toString())}
+          >
+            <span
+              className={`inline-block ${
+                selectedCategory === category
+                  ? 'border-b-2 border-black pb-1 font-bold'
+                  : ''
+              }`}
+            >
+              {category}
+            </span>
+          </button>
+        ))}
+      </div>
 
       {/* Dropdown Menu for Mobile */}
       <Disclosure
         as="div"
-        className="w-full md:w-72 z-1 sticky top-28 md:top-32 md:hidden"
+        className="sticky top-28 z-1 w-full md:top-32 md:hidden md:w-72"
       >
         {({ open }) => (
           <>
-            <DisclosureButton className="w-full text-left py-3 px-5 rounded-full border-1 bg-white bodyMD flex justify-between">
-              {selectedCategory || "Select Category"}
+            <DisclosureButton className="bodyMD flex w-full justify-between rounded-full border-1 bg-white px-5 py-3 text-left">
+              {selectedCategory || 'Select Category'}
               <ChevronDownIcon
                 className={`h-6 transition-transform duration-300${
-                  open ? "rotate-180" : ""
+                  open ? 'rotate-180' : ''
                 }`}
               />
             </DisclosureButton>
@@ -90,12 +113,12 @@ export default function Page({
               leaveFrom="opacity-100 translate-y-0"
               leaveTo="opacity-0 -translate-y-2"
             >
-              <DisclosurePanel className="flex flex-col gap-5 absolute w-full mt-2 bg-white border rounded-[2rem] p-5 items-start">
+              <DisclosurePanel className="absolute mt-2 flex w-full flex-col items-start gap-5 rounded-[2rem] border bg-white p-5">
                 {categories.map((category) => (
                   <CloseButton
                     key={category.toString()}
-                    className={`cursor-pointer bodyMD ${
-                      selectedCategory === category ? "font-bold" : ""
+                    className={`bodyMD cursor-pointer ${
+                      selectedCategory === category ? 'font-bold' : ''
                     }`}
                     onClick={() => handleCategoryClick(category.toString())}
                   >
@@ -109,16 +132,16 @@ export default function Page({
       </Disclosure>
 
       {/* FAQ List */}
-      <div className="mt-6 mb-32 w-full flex flex-col gap-8 items-center">
+      <div className="mt-6 mb-32 flex w-full md:w-fit flex-col items-center gap-8">
         {Object.entries(groupedFaqs).map(([category, faqList]) => (
           <div
             key={category}
             ref={(el: HTMLDivElement | null) => {
               categoryRefs.current[category] = el;
             }}
-            className="flex flex-col max-w-3xl justify-center p-6 bg-white rounded-xl md:rounded-[1.25rem] drop-shadow-[4px_4px_0px_rgba(228,223,209,1)] w-full"
+            className="flex w-full max-w-3xl flex-col justify-center rounded-xl bg-white p-6 drop-shadow-[4px_4px_0px_rgba(228,223,209,1)] md:rounded-[1.25rem]"
           >
-            <p className="bodyXL text-gray-500 mb-4">{category}</p>
+            <p className="bodyXL mb-4 text-gray-500">{category}</p>
 
             {/* Render all FAQs for the category */}
             {faqList.map((faq, index) => (
@@ -134,8 +157,8 @@ export default function Page({
                         {faq.fields.question.toString()}
                       </span>
                       <ChevronDownIcon
-                        className={`size-8 transition-all text-[#2C2420] min-w-9 ${
-                          open ? "rotate-180" : ""
+                        className={`size-8 min-w-9 text-[#2C2420] transition-all ${
+                          open ? 'rotate-180' : ''
                         }`}
                       />
                     </DisclosureButton>
@@ -147,7 +170,7 @@ export default function Page({
                       leaveFrom="opacity-100 translate-y-0"
                       leaveTo="opacity-0 -translate-y-2"
                     >
-                      <DisclosurePanel className="bodyMD text-gray-700 pb-4">
+                      <DisclosurePanel className="bodyMD pb-4 text-gray-700">
                         {faq.fields.answer.toString()}
                       </DisclosurePanel>
                     </Transition>
@@ -157,7 +180,7 @@ export default function Page({
             ))}
           </div>
         ))}
-        <ScrollUpButton className="fixed bottom-6 right-10" />
+        <ScrollUpButton className="fixed right-10 bottom-6" />
       </div>
     </div>
   );
