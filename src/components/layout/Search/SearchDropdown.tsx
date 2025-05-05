72868d5907f262968cf57986826482707df7e1ac
Darren Teo HH <darrenteo77@gmail.com>
2025-05-05 17:59:10 +0800

    Ipad fix

--- src/components/layout/Search/SearchDropdown.tsx
+++ src/components/layout/Search/SearchDropdown.tsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from "react";
-import SearchInput from "./SearchInput";
-import { getProductsBySearch } from "@/utils/APIs";
-import { ShopfrontProduct } from "@/types/Types";
-import ProductSearchResult from "./ProductSearchResult";
+import React, { useState, useEffect } from 'react';
+import SearchInput from './SearchInput';
+import { getProductsBySearch } from '@/utils/APIs';
+import { ShopfrontProduct } from '@/types/Types';
+import ProductSearchResult from './ProductSearchResult';
 
 export default function SearchDropdown({
   handleClose,
-  isOpen,
+  isOpen
 }: {
   handleClose: () => void;
   isOpen: boolean;
 }) {
-  const [searchKey, setSearchKey] = useState<string>("");
+  const [searchKey, setSearchKey] = useState<string>('');
   const [searchResults, setSearchResults] = useState<ShopfrontProduct[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -27,7 +27,7 @@ export default function SearchDropdown({
         const results = await getProductsBySearch(searchKey);
         setSearchResults(results);
       } catch (error) {
-        console.error("Search failed:", error);
+        console.error('Search failed:', error);
       } finally {
         setIsLoading(false);
       }
@@ -39,14 +39,16 @@ export default function SearchDropdown({
   return (
     <>
       <div
-        className={`w-screen h-screen bg-primary/50 absolute z-20 ${
-          !isOpen ? "hidden" : "block"
+        className={`bg-primary/50 fixed inset-0 z-20 ${
+          !isOpen ? 'hidden' : 'block'
         }`}
         onClick={handleClose}
       />
       <div
-        className={`fixed top-0 left-0 md:right-0 md:left-auto w-full sm:w-96 h-screen bg-yellow-pastel z-30 flex flex-col items-center p-8 transition-all duration-300 ${
-          isOpen ? "-translate-x-0 md:translate-x-0" : "-translate-x-full md:translate-x-full"
+        className={`bg-yellow-pastel fixed top-0 left-0 z-30 flex h-screen w-full flex-col items-center p-8 transition-all duration-300 sm:w-96 xl:right-0 xl:left-auto ${
+          isOpen
+            ? '-translate-x-0 xl:translate-x-0'
+            : '-translate-x-full xl:translate-x-full'
         }`}
       >
         <SearchInput
@@ -55,12 +57,12 @@ export default function SearchDropdown({
           handleClose={handleClose}
         />
         {isLoading && <div>Searching...</div>}
-        <div className="flex flex-col divide-y divide-gray-200 w-full mt-8">
+        <div className="mt-8 flex w-full flex-col divide-y divide-gray-200">
           {searchResults.map((product) => (
             <ProductSearchResult key={product.id} product={product} />
           ))}
           {searchResults.length === 0 && searchKey.length > 2 && !isLoading && (
-            <div className="bodyMD text-gray-800">No results found</div>
+            <div className="bodyxl text-gray-800">No results found</div>
           )}
         </div>
       </div>
