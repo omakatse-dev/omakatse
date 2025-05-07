96f05acf169c9b176f90248132d14c48e3d9dda3
Darren Teo HH <darrenteo77@gmail.com>
2025-05-07 13:05:03 +0800

    Add padding right to subscription cards' fields

--- src/components/account/subscriptions/SubscriptionCard.tsx
+++ src/components/account/subscriptions/SubscriptionCard.tsx
@@ -1,38 +1,39 @@
-"use client";
+'use client';
 
-import Button from "@/components/common/Button";
-import Card from "@/components/common/Card";
-import ProgressBar from "@/components/subscription/ProgressBar";
-import Pets from "./Pets";
+import dayjs from 'dayjs';
+import Button from '@/components/common/Button';
+import Card from '@/components/common/Card';
+import ProgressBar from '@/components/subscription/ProgressBar';
+import Pets from './Pets';
 
-import { SubscriptionContract } from "@/types/Types";
-import { petDetailsSchema } from "@/schemas/SubscriptionFormSchema";
-import { z } from "zod";
-import Tag from "@/components/common/Tag";
-import { useRouter } from "next/navigation";
+import { SubscriptionContract } from '@/types/Types';
+import { petDetailsSchema } from '@/schemas/SubscriptionFormSchema';
+import { z } from 'zod';
+import Tag from '@/components/common/Tag';
+import { useRouter } from 'next/navigation';
 
 export type PetType = z.infer<typeof petDetailsSchema>;
 
 export default function SubscriptionCard({
-  subscription,
+  subscription
 }: {
   subscription: SubscriptionContract;
 }) {
   const pets = JSON.parse(subscription.pets);
-  const dogs = pets.filter((pet: PetType) => pet.type === "Dog");
-  const cats = pets.filter((pet: PetType) => pet.type === "Cat");
-  console.log(cats)
+  const dogs = pets.filter((pet: PetType) => pet.type === 'Dog');
+  const cats = pets.filter((pet: PetType) => pet.type === 'Cat');
+  console.log(cats);
   const contractId = subscription.contractId;
 
   const router = useRouter();
 
   return (
-    <Card className="bg-white flex flex-col gap-6 md:gap-8">
-      <div className="flex flex-col-reverse md:flex-row justify-between gap-4">
+    <Card className="flex flex-col gap-6 bg-white md:gap-8">
+      <div className="flex flex-col-reverse justify-between gap-4 md:flex-row">
         <Pets dogs={dogs} cats={cats} />
         <Tag
           className={`h-fit w-fit ${
-            subscription.status === "ACTIVE" ? "bg-yellow" : "bg-gray-200"
+            subscription.status === 'ACTIVE' ? 'bg-yellow' : 'bg-gray-200'
           }`}
         >
           {subscription.status}
@@ -40,77 +41,77 @@ export default function SubscriptionCard({
       </div>
       <div className="flex flex-col items-center gap-2">
         <div className="bodyMD font-semibold text-gray-800">
-          Box{" "}
-          {subscription.boxIds?.split(",").length % subscription.planDuration}{" "}
+          Box{' '}
+          {subscription.boxIds?.split(',').length % subscription.planDuration}{' '}
           out of {subscription.planDuration}
         </div>
         <ProgressBar
           showSteps={false}
           currentStep={
-            subscription.boxIds?.split(",").length % subscription.planDuration
+            subscription.boxIds?.split(',').length % subscription.planDuration
           }
           totalSteps={subscription.planDuration}
         />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        <div className="md:border-r border-gray-200">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+        <div className="border-gray-200 md:border-r pr-8">
           <label className="bodySM text-gray-500">Box size</label>
-          <div className="bodyMD font-semibold mt-1">{subscription.size}</div>
+          <div className="bodyMD mt-1 font-semibold">{subscription.size}</div>
         </div>
-        <div className="md:border-r border-gray-200">
+        <div className="border-gray-200 md:border-r pr-8">
           <label className="bodySM text-gray-500">No. of Pets</label>
-          <div className="bodyMD font-semibold mt-1">
-            {dogs.length > 0 && `${dogs.length} dog(s)`}{" "}
+          <div className="bodyMD mt-1 font-semibold">
+            {dogs.length > 0 && `${dogs.length} dog(s)`}{' '}
             {cats.length > 0 && `${cats.length} cat(s)`}
           </div>
         </div>
-        <div>
+        <div className="pr-8">
           <label className="bodySM text-gray-500">Next billing date</label>
-          <div className="bodyMD font-semibold mt-1">
-            {subscription.nextBillingDate}
+          <div className="bodyMD mt-1 font-semibold">
+            {dayjs(subscription.nextBillingDate).format('MMMM DD, YYYY')}
           </div>
         </div>
-        <div className="md:border-r border-gray-200">
+        <div className="border-gray-200 md:border-r pr-8">
           <label className="bodySM text-gray-500">Ship to</label>
-          <div className="bodyMD font-semibold mt-1">{subscription.name}</div>
+          <div className="bodyMD mt-1 font-semibold">{subscription.name}</div>
         </div>
-        <div className="md:border-r border-gray-200">
+        <div className="border-gray-200 md:border-r pr-8">
           <label className="bodySM text-gray-500">Shipping address</label>
-          <div className="bodyMD font-semibold mt-1">
+          <div className="bodyMD mt-1 font-semibold">
             {subscription.address}
           </div>
         </div>
-        <div>
+        <div className="pr-8">
           <label className="bodySM text-gray-500">Shipping method</label>
-          <div className="bodyMD font-semibold mt-1">Standard shipping</div>
+          <div className="bodyMD mt-1 font-semibold">Standard shipping</div>
         </div>
-        <div className="md:border-r border-gray-200">
+        <div className="border-gray-200 md:border-r pr-8">
           <label className="bodySM text-gray-500">Box plan</label>
-          <div className="bodyMD font-semibold mt-1">
+          <div className="bodyMD mt-1 font-semibold">
             {subscription.planDuration} months
           </div>
         </div>
-        <div className="md:border-r border-gray-200">
+        <div className="border-gray-200 md:border-r pr-8">
           <label className="bodySM text-gray-500">Monthly payment</label>
-          <div className="bodyMD font-semibold mt-1">AED 25</div>
+          <div className="bodyMD mt-1 font-semibold">AED 25</div>
         </div>
-        <div>
+        <div className="pr-8">
           <label className="bodySM text-gray-500">Next Renewal Date</label>
-          <div className="bodyMD font-semibold mt-1">
-            {subscription.nextRenewalDate}
+          <div className="bodyMD mt-1 font-semibold">
+            {dayjs(subscription.nextRenewalDate).format('MMMM DD, YYYY')}
           </div>
         </div>
       </div>
-      {subscription.status === "ACTIVE" ? (
+      {subscription.status === 'ACTIVE' ? (
         <Button
           onClick={() =>
             router.push(
               `/exit-survey?contractId=${contractId}&email=${subscription.email}`
             )
           }
-          className="w-full md:w-fit self-center"
+          className="w-full self-center md:w-fit"
           disabled={
-            subscription.boxIds?.split(",").length !== subscription.planDuration
+            subscription.boxIds?.split(',').length !== subscription.planDuration
           }
         >
           Cancel auto-renew
@@ -120,13 +121,13 @@ export default function SubscriptionCard({
           onClick={() =>
             router.push(`/renew-subscription?contractId=${contractId}`)
           }
-          className="w-full md:w-fit self-center"
+          className="w-full self-center md:w-fit"
         >
           Renew subscription
         </Button>
       )}
 
-      <ul className="list-disc list-inside bodySM text-gray-500">
+      <ul className="bodySM list-inside list-disc text-gray-500">
         <li>
           Your subscription will automatically renew after your current billing
           cycle ends.
