5596656091d38f6af993b5813491aa8f2843bf33
Darren Teo HH <darrenteo77@gmail.com>
2025-05-13 12:58:12 +0800

    Bug Fixes

--- src/components/account/pet-profiles/EditPetSuccessModal.tsx
+++ src/components/account/pet-profiles/EditPetSuccessModal.tsx
@@ -9,7 +9,7 @@ export default function EditPetSuccessModal() {
     <div className="fixed inset-0 flex items-center justify-center bg-black/60 z-20">
       <div className="bg-yellow-pastel p-6 sm:py-8 sm:px-12 rounded-2xl flex flex-col gap-6 sm:gap-8 max-w-72 text-center sm:max-w-none">
         <h4>Your pet profile has been updated!</h4>
-        <Button onClick={() => router.push("/account/pet-profiles")}>
+        <Button onClick={() => router.back()}>
           Back to pet profile
         </Button>
       </div>
