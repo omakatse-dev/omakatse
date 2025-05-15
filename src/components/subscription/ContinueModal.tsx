7871b5fe7f05c61d68ba28c5c08097ce182f00e9
matthew-chua <mattchua2603@gmail.com>
2025-05-15 16:49:17 +0800

    removed x

--- src/components/subscription/ContinueModal.tsx
+++ src/components/subscription/ContinueModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Button from '../common/Button';
-import { XMarkIcon } from '@heroicons/react/24/outline';
 import { useSubscriptionFormStore } from '@/stores/subscriptionFormStore';
 import { useRouter } from 'next/navigation';
 
@@ -24,10 +23,6 @@ export default function ContinueModal({ close }: { close: () => void }) {
         className="bg-yellow-pastel relative flex w-11/12 max-w-xl flex-col items-center gap-8 rounded-2xl p-6 pt-10 text-center sm:w-full sm:px-16 sm:py-8"
         onClick={(e) => e.stopPropagation()}
       >
-        <XMarkIcon
-          className="absolute top-4 right-4 h-6 w-6 cursor-pointer stroke-2"
-          onClick={close}
-        />
         <h4>Would you like to resume your subscription journey?</h4>
         <div className="flex w-full flex-col gap-2 sm:flex-row sm:gap-4">
           <Button
